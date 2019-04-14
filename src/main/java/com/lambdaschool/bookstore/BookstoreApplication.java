f08516cadccb340b50d422e66798d9ddebae6663
johnoro <officialjohnorourke@gmail.com>
2019-04-14 17:53:09 -0400

    Bootstraps project with needed dependencies

---
+++ src/main/java/com/lambdaschool/bookstore/BookstoreApplication.java
@@ -0,0 +1,11 @@
+package com.lambdaschool.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(BookstoreApplication.class, args);
+  }
+}
