d8b6ad77bca18dda89d825104853a7450ce2a22c
johnoro <officialjohnorourke@gmail.com>
2019-04-14 23:22:40 -0400

    Adds custom swagger documentation where required

--- src/main/java/com/lambdaschool/bookstore/controllers/GetController.java
+++ src/main/java/com/lambdaschool/bookstore/controllers/GetController.java
@@ -6,20 +6,32 @@ import com.lambdaschool.bookstore.models.Section;
 import com.lambdaschool.bookstore.repositories.AuthorRepository;
 import com.lambdaschool.bookstore.repositories.BookRepository;
 import com.lambdaschool.bookstore.repositories.SectionRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(
+  description = "Contains GET endpoints for authors, books, and sections"
+)
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class GetController {
   @Autowired
   BookRepository bookRepository;
 
-  @RequestMapping(value = "books")
+  @ApiOperation(value = "Lists all books or an empty list")
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Successfully retrieved books")
+  })
+  @GetMapping(value = "books")
   List<Book> findAllBooks() {
     return bookRepository.findAll();
   }
@@ -27,7 +39,11 @@ public class GetController {
   @Autowired
   AuthorRepository authorRepository;
 
-  @RequestMapping(value = "authors")
+  @ApiOperation(value = "Lists all authors or an empty list")
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Successfully retrieved authors")
+  })
+  @GetMapping(value = "authors")
   List<Author> findAllAuthors() {
     return authorRepository.findAll();
   }
@@ -35,7 +51,11 @@ public class GetController {
   @Autowired
   SectionRepository sectionRepository;
 
-  @RequestMapping(value = "sections")
+  @ApiOperation(value = "Lists all sections or an empty list", response = List.class)
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Successfully retrieved sections")
+  })
+  @GetMapping(value = "sections")
   List<Section> findAllSections() {
     return sectionRepository.findAll();
   }
