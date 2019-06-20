def47a14271ce259e0550c6541399b44d6299acc
jrmmba8314 <jrmmba@outlook.com>
2019-06-20 14:25:08 -0700

    Changing Start Project

--- java-bookstore/src/main/java/com/lambdaschool/bookstore/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lambdaschool.bookstore.repository;
-
-import com.lambdaschool.bookstore.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long>
-{
-    User findByUsername(String username);
-}
