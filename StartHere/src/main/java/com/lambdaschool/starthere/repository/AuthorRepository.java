8f4a032c08912f4fe63673b7f3345448cec628d5
atolmie <atolmie22@gmail.com>
2019-06-28 11:48:46 -0500

    added repos

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.starthere.repository;
+
+import com.lambdaschool.starthere.models.Author;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long>
+{
+
+}
---
+++ StartHere/src/main/java/com/lambdaschool/starthere/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.starthere.repository;
+
+import com.lambdaschool.starthere.models.Book;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BookRepository extends PagingAndSortingRepository<Book, Long>
+{
+
+}
