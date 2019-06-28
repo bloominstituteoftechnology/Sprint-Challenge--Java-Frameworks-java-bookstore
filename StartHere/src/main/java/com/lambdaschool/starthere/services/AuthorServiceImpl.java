5761ad2afbe7991dd2e2660d3787a71d6e2cb31a
atolmie <atolmie22@gmail.com>
2019-06-28 12:06:32 -0500

    added services

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/AuthorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.starthere.services;
+
+import com.lambdaschool.starthere.models.Author;
+import com.lambdaschool.starthere.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService
+{
+    @Autowired
+    private AuthorRepository repo;
+
+
+    @Override
+    public List<Author> findAll() {
+        List<Author> authorList = new ArrayList<>();
+        repo.findAll().iterator().forEachRemaining(authorList::add);
+        return authorList;
+    }
+}
---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/BookService.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.starthere.services;
+
+import com.lambdaschool.starthere.models.Book;
+
+import java.util.List;
+
+public interface BookService
+{
+    List<Book> findAll();
+
+    Book updateBook(Book book, long id);
+
+    void delete(long id);
+
+    void assignAuthor(long bookid, long authorid);
+}
