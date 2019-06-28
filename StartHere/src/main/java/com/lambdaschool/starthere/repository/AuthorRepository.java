dcb7087c4a2b6688b9e09098b504121181431cda
unknown <bejolo018@gmail.com>
2019-06-28 12:33:22 -0500

    added author and book services/repository/controllers

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.starthere.repository;
+
+import com.lambdaschool.starthere.models.Author;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+
+}
