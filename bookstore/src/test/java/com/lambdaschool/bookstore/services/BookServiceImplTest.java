628f47c55db0b779f02a3babd3889db84081cccf
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-13 11:13:30 -0700

    Tests completed for Book Service Unit Test

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
 //**********
@@ -38,26 +41,38 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
+    assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        assertEquals("Digital Fortress", bookService.findBookById(2).getName());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
+        assertEquals("Digital Fortress", bookService.findBookById(202).getName());
     }
 
     @Test
     public void delete()
     {
+        bookService.delete(3);
+        assertEquals(2, bookService.findAll().size());
     }
 
     @Test
     public void save()
     {
+        String b1Name = "this testing book";
+        Book b1 = new Book("Flatterland", "9780738206752", 2001, s1);
+        b1.setWrotes(wrote);
+
+        Book addBook = bookService.save(b1);
+        assertNotNull(addBook);
+        assertEquals(b1Name, addBook.getName());
     }
 
     @Test
