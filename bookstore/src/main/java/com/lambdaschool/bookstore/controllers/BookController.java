16663ad506ef66b9065869865f3b87cf9c292552
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-18 09:27:06 -0700

    security added for book routes

--- bookstore/src/main/java/com/lambdaschool/bookstore/controllers/BookController.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/controllers/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,31 +31,34 @@ public class BookController
     BookService bookService;
 
     // http://localhost:2019/books/books
+    @PreAuthorize(value = "hasAnyRole('ADMIN', 'DATA')")
     @GetMapping(value = "/books",
-            produces = {"application/json"})
+        produces = {"application/json"})
     public ResponseEntity<?> listAllBooks(HttpServletRequest request)
     {
         List<Book> myBooks = bookService.findAll();
         return new ResponseEntity<>(myBooks,
-                                    HttpStatus.OK);
+            HttpStatus.OK);
     }
 
     // http://localhost:2019/books/book/{bookId}
+    @PreAuthorize(value = "hasAnyRole('ADMIN', 'DATA')")
     @GetMapping(value = "/book/{bookId}",
-            produces = {"application/json"})
+        produces = {"application/json"})
     public ResponseEntity<?> getBookById(HttpServletRequest request,
                                          @PathVariable
-                                                 Long bookId)
+                                             Long bookId)
     {
         Book s = bookService.findBookById(bookId);
         return new ResponseEntity<>(s,
-                                    HttpStatus.OK);
+            HttpStatus.OK);
     }
 
     // POST http://localhost:2019/books/book
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
     @PostMapping(value = "/book", consumes = "application/json")
     public ResponseEntity<?> addNewBook(@Valid @RequestBody Book newBook) throws
-            URISyntaxException
+                                                                          URISyntaxException
     {
         newBook.setBookid(0);
         newBook = bookService.save(newBook);
@@ -62,25 +66,26 @@ public class BookController
         // set the location header for the newly created resource
         HttpHeaders responseHeaders = new HttpHeaders();
         URI newBookURI = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{bookid}")
-                .buildAndExpand(newBook.getBookid())
-                .toUri();
+            .path("/{bookid}")
+            .buildAndExpand(newBook.getBookid())
+            .toUri();
         responseHeaders.setLocation(newBookURI);
 
         return new ResponseEntity<>(null,
-                                    responseHeaders,
-                                    HttpStatus.CREATED);
+            responseHeaders,
+            HttpStatus.CREATED);
     }
 
     // PUT http://localhost:2019/books/book/1
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
     @PutMapping(value = "/book/{bookid}",
-            consumes = "application/json")
+        consumes = "application/json")
     public ResponseEntity<?> updateFullBook(
-            @Valid
-            @RequestBody
-                    Book updateBook,
-            @PathVariable
-                    long bookid)
+        @Valid
+        @RequestBody
+            Book updateBook,
+        @PathVariable
+            long bookid)
     {
         updateBook.setBookid(bookid);
         bookService.save(updateBook);
@@ -89,10 +94,11 @@ public class BookController
     }
 
     // DELETE http://localhost:2019/books/book/1
+    @PreAuthorize(value = "hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/book/{id}")
     public ResponseEntity<?> deleteBookById(
-            @PathVariable
-                    long id)
+        @PathVariable
+            long id)
     {
         bookService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
