cb9d3fb1b3c9c5d46aacae88ee7017c4a44bf62f
kai-blt <blaitong@gmail.com>
2020-12-18 08:08:46 -0800

    added security to books routes

--- bookstore/src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.bookstore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -57,6 +58,10 @@ public class ResourceServerConfig
                              "/oauth/revoke-token",
                              "/logout")
                 .authenticated()
+                .antMatchers(HttpMethod.GET, "/books/books/**").hasAnyRole("ADMIN", "DATA")
+                .antMatchers(HttpMethod.POST, "/books/book/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/books/book/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/books/book/**").hasAnyRole("ADMIN")
                 .antMatchers("/roles/**")
                 .hasAnyRole("ADMIN", "DATA")
                 .and()
